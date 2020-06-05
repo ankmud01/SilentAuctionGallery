d30d1a80542348b8a4c6c25f7088e6317d47d1d6
Steven Dean <sdlanglois1@gmail.com>
2020-06-05 17:06:11 -0400

    added model for school

---
+++ models/school.js
@@ -0,0 +1,36 @@
+
+// Creating our school model
+module.exports = function bar(sequelize, DataTypes) {
+  const School = sequelize.define('School', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    schoolName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    address: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    state: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    city: {
+      type: DataTypes.STRING,
+    },
+    zip: {
+      type: DataTypes.STRING,
+    },
+    adminName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      },
+    },
+  );
+  return School;
+};
