e63aed5b1d26d6285a4271ad0d0183215b7bcbe9
Rebel Soul <mudvari.ankit@gmail.com>
2020-06-13 11:05:54 -0400

    commit 1

--- controllers/profile_controller.js
+++ controllers/profile_controller.js
@@ -17,7 +17,9 @@ router.get('/members' /* '/api/user' */, (req, res) => {
         isloggedin: req.isAuthenticated(),
       };
       console.log(user.userInfo);
-      res.render('members', user);
+      res.render('members', {
+        userInfo: user.userInfo, title: 'Profile Page', school: 'North Oconee High School', logged: req.isAuthenticated(),
+      });
     });
   } else {
     // eslint-disable-next-line no-unused-vars
@@ -27,6 +29,44 @@ router.get('/members' /* '/api/user' */, (req, res) => {
     };
     res.redirect('/');
   }
+});
+
+// ROUTER TO DELETE ACCOUNT
+router.delete('/user/:account_id/:password', (req, res) => {
+  console.log(`id: ${req.params.account_id}`);
+  console.log(`Password: ${req.params.password}`);
+  // db.user.destroy({
+  //   where: {
+  //     id: req.params.account_id,
+  //     account_key: req.params.account_key,
+  //   },
+  // }).then((dbuser) => {
+  //   res.json(dbuser);
+  // });
+});
+
+// ROUTER TO UPDATE ACCOUNT
+router.put('/user/:account_id/:password', (req, res) => {
+  db.user.update({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    address: req.body.address1,
+    address2: req.body.address2,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    school: req.body.school,
+    email: req.body.email,
+    phone: req.body.phone,
+    password: req.body.password,
+  }, {
+    where: {
+      id: req.params.account_id,
+      password: req.params.password,
+    },
+  }).then((dbuser) => {
+    res.json(dbuser);
+  });
 });
 
 module.exports = router;
